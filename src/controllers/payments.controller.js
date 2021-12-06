cc9515b0fce1e48f0820c44c18ebfa3e6dd9fa36
Sebastian <seborbon@outlook.com>
2021-12-06 18:53:36 -0500

    controllers finished

--- src/controllers/payments.controller.js
+++ src/controllers/payments.controller.js
@@ -1,31 +1,97 @@
 import axios from "axios";
+import {
+  PAYPAL_API,
+  PAYPAL_API_CLIENT,
+  PAYPAL_API_SECRET,
+  HOST,
+} from "../../config";
 
 export const createOrder = async (req, res) => {
-  const order = {
-    intent: "CAPTURE",
-    purchase_units: [
-      {
-        amount: {
-          currency_code: "USD",
-          value: "90,5",
+  try {
+    const order = {
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: "150",
+          },
+          description: "mechanical keyboard",
         },
-        description: "mechanical keyboard",
+      ],
+      application_context: {
+        brand_name: "SebasCompany",
+        landing_page: "NO_PREFERENCE",
+        user_action: "PAY_NOW",
+        return_url: `${HOST}/capture-order`,
+        cancel_url: `${HOST}/cancel-payment`,
       },
-    ],
-    aplicacion_context: {
-      brand_name: "sebasCompany",
-      landing_page: "LOGIN",
-      user_action: "PAY_NOW",
-      return_url: "http://localhost:3000/capture-order",
-      cancel_url: "http://localhost:3000/cancel-order",
-    },
-  };
+    };
+
+    // format the body
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+
+    // Generate an access token
+    const {
+      data: { access_token },
+    } = await axios.post(
+      "https://api-m.sandbox.paypal.com/v1/oauth2/token",
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        auth: {
+          username: PAYPAL_API_CLIENT,
+          password: PAYPAL_API_SECRET,
+        },
+      }
+    );
+
+    console.log(access_token);
+
+    // make a request
+    const response = await axios.post(
+      `${PAYPAL_API}/v2/checkout/orders`,
+      order,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    return res.json(response.data);
+  } catch (error) {
+    return res.status(500).send("Something goes wrong");
+  }
 };
 
-export const captureOrder = (req, res) => {
-  res.send("capturing an order");
+export const captureOrder = async (req, res) => {
+  const { token } = req.query;
+
+  try {
+    const response = await axios.post(
+      `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+      {},
+      {
+        auth: {
+          username: PAYPAL_API_CLIENT,
+          password: PAYPAL_API_SECRET,
+        },
+      }
+    );
+
+    console.log(response.data);
+
+    res.redirect("/payed.html");
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal Server error" });
+  }
 };
 
 export const cancelOrder = (req, res) => {
-  res.send("cancel an order");
+  res.redirect("/");
 };
