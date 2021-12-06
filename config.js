6df54e0363101afe483c342c8c1133c1883dcfd2
Sebastian <seborbon@outlook.com>
2021-12-05 23:46:12 -0500

    config the account credentials

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PAYPAL_API_CLIENT =
+  "AdyWPMF-iUxKC3O_Qk3mqo_y8uSb0zbB1g7BAlnyIm0iMb6b8AoueKu7e4iaHRKodsoUr0TR-7boZODv";
+export const PAYPAL_API_SECRET =
+  "EBP-Z9MQFyg72rUhex1IbrqtiRFAB5dLISE26ar5Ck7kcfi49Jts2vFtpPIAy3IDAgHYr9iNOqwZw6_e";
+export const PAYPAL_API = "https://api-m.sandbox.paypal.com";
