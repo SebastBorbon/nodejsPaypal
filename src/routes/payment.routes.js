4392a624897eaa813c128f0013b33c0cfa250bb9
Sebastian <seborbon@outlook.com>
2021-12-05 23:45:50 -0500

    create payment routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import {
+  createOrder,
+  captureOrder,
+  cancelOrder,
+} from "../controllers/payments.controller";
 
 const router = Router();
+
+router.get("/create-order", createOrder);
+router.get("/capture-order", captureOrder);
+router.get("/cancel-order", cancelOrder);
+
+export default router;
