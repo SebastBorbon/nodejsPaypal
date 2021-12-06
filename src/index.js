73de61a033653612efc11fcae48d3a26fdaa8001
Sebastian <seborbon@outlook.com>
2021-12-06 02:51:02 -0500

    create rutes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import morgan from "morgan";
+import paymentRoutes from "./routes/payment.routes";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("dev"));
+
+app.use(paymentRoutes);
 
 app.listen(port);
 console.log("server on port", port);
